9fcab6e78077d2f4973c26be385b0432c71019ce
anacmaciel <casmaciel96@gmail.com>
2022-11-11 19:51:42 -0300

    subindo jogo com for, break e continue

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,21 +3,24 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("**********")
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
-while (rodada <= total_de_tentativas):
+for rodada in range (1,total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite um número")
+    chute_str = input("Digite um número entre 1 e 100:")
     print("você digitou: ", chute_str)
     chute = int(chute_str)
+
+    if(chute > 1 or chute < 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
     if (acertou):
         print("você acertou!")
+        break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto!")
         elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto!")
-    total_de_tentativas = total_de_tentativas - 1
-    print("fim do jogo")
+print("fim do jogo")
---
+++ exercicio_for.py
@@ -0,0 +1,2 @@
+for rodada in  [1,2,3,4,5]:
+    print(rodada)
