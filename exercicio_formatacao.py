6d44468e6766f21adc24acec2800a183e8ac4da8
anacmaciel <casmaciel96@gmail.com>
2022-11-11 21:02:09 -0300

    subindo exercicios auxiliares e lógica do jogo sem número randômico

---
+++ exercicio_formatacao.py
@@ -0,0 +1,13 @@
+print("R$ {:07.2f}\n".format(1.5))
+
+print("R$ {:7.2f}\n".format(1234.50))
+
+print("R$ {:07d}\n".format(4))
+
+print("Data: {:02d}/{:02d}\n".format(9, 4))
+
+print("Data: {:02d}/{:02d}".format(19, 11))
+nome = "Matheus"
+print(f'meu nome é {nome}')
+
+print(f'Meu nome é {nome.lower()}')
